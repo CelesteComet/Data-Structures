2a1a9a93855a29bb91459b1b32d5cc3c63f48daf
bvluong <bryantvinluong@gmail.com>
2017-06-19 21:17:38 -0700

    Add toplogicalSort

--- graph_topo/lib/graph.rb
+++ graph_topo/lib/graph.rb
@@ -6,6 +6,15 @@ class Vertex
     @in_edges = []
     @out_edges = []
   end
+
+  def remove_in_edge(edge)
+    @in_edges.delete(edge)
+  end
+
+  def remove_out_edge(edge)
+    @out_edges.delete(edge)
+  end
+
 end
 
 class Edge
@@ -24,6 +33,9 @@ class Edge
   end
 
   def destroy!
-
+    @from_vertex.remove_out_edge(self)
+    @to_vertex.remove_in_edge(self)
+    @from_vertex = nil
+    @to_vertex = nil
   end
 end
--- graph_topo/lib/topological_sort.rb
+++ graph_topo/lib/topological_sort.rb
@@ -3,5 +3,28 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  p vertices.map {|v| v.value}
+  sorted = []
+  queue = []
+  vertices.each do |vert|
+    queue << vert if vert.in_edges.length == 0
+  end
+  until queue.empty?
+    p 'queue'
+    p queue.map {|v| v.value}
+    current = queue.pop
+    sorted << current
+    neighbor_verts = []
+    current.out_edges.each do |edge|
+      neighbor_verts << edge.to_vertex
+      edge.destroy!
+    end
+    p neighbor_verts.map {|v| v}
+    neighbor_verts.each do |vert|
+      queue.unshift(vert) if vert.in_edges.length == 0
+    end
+  end
+  p sorted.map {|v| v.value}
+  sorted
 
 end

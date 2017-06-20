29f91f19a75538553788d45eaab3191de4e3414b
bvluong <bryantvinluong@gmail.com>
2017-06-19 21:36:25 -0700

    Add fix due to destroying elements iterating through array

--- graph_topo/lib/topological_sort.rb
+++ graph_topo/lib/topological_sort.rb
@@ -3,28 +3,22 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
-  p vertices.map {|v| v.value}
   sorted = []
   queue = []
   vertices.each do |vert|
     queue << vert if vert.in_edges.length == 0
   end
   until queue.empty?
-    p 'queue'
-    p queue.map {|v| v.value}
     current = queue.pop
     sorted << current
-    neighbor_verts = []
+    edges = []
     current.out_edges.each do |edge|
-      neighbor_verts << edge.to_vertex
-      edge.destroy!
+      queue.unshift(edge.to_vertex) if edge.to_vertex.in_edges.length == 1
+      edges << edge
     end
-    p neighbor_verts.map {|v| v}
-    neighbor_verts.each do |vert|
-      queue.unshift(vert) if vert.in_edges.length == 0
-    end
+    edges.each {|edge| edge.destroy! }
   end
-  p sorted.map {|v| v.value}
+  return [] unless sorted.length == vertices.length
   sorted
 
 end

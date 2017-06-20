ddff1e8c109d638830456f396fe3a6d7589f0af9
bvluong <bryantvinluong@gmail.com>
2017-06-19 20:47:39 -0700

    Initialize Vertices and Edges

--- graph_topo/lib/graph.rb
+++ graph_topo/lib/graph.rb
@@ -1,12 +1,26 @@
 class Vertex
-  def initialize(value)
+  attr_accessor :in_edges, :out_edges, :value
 
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
+  attr_accessor :from_vertex, :to_vertex, :cost
 
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    add_self
+  end
+
+  def add_self
+    @from_vertex.out_edges << self
+    @to_vertex.in_edges << self
   end
 
   def destroy!

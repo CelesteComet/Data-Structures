fe7df4cfcbd29e79745f3be8f5801540fdf1e5f0
bvluong <bryantvinluong@gmail.com>
2017-06-20 21:14:53 -0700

    Add bottom up solution to frog_hops

--- dynamic_programming/lib/dynamic_programming.rb
+++ dynamic_programming/lib/dynamic_programming.rb
@@ -5,16 +5,26 @@ class DynamicProgramming
 
   def blair_nums(n)
     return @blair_cache[n] if @blair_cache[n]
-    third = (n.odd? ? n : n+1)
-    ans = blair_nums(n-1) + blair_nums(n-2) + third
+    ans = blair_nums(n-1) + blair_nums(n-2) + ((n-1)*2+1)
     @blair_cache[n] = ans
     @blair_cache[n]
   end
 
   def frog_hops(n)
+    cache = frog_cache_builder(n)
+    cache[n]
   end
 
   def frog_cache_builder(n)
+    cache = {1=>[[1]], 2=>[[1,1],[2]], 3=>[[1, 1, 1], [1, 2], [2, 1],[3]]}
+    return cache if n <= 3
+    (4..n).step do |idx|
+      cache_1 = cache[idx-1].map {|arr| arr + [1] }
+      cache_2 = cache[idx-2].map {|arr| arr + [2] }
+      cache_3 = cache[idx-3].map {|arr| arr + [3] }
+      cache[idx] = cache_1 + cache_2 + cache_3
+    end
+    cache
   end
 
   def frog_hops_top_down(n)

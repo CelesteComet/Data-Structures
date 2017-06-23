895e5948f0de44966a08670890c188461b2690f2
bvluong <bryantvinluong@gmail.com>
2017-06-22 20:48:59 -0700

    Add heapify_down function

--- heap/lib/heap.rb
+++ heap/lib/heap.rb
@@ -2,6 +2,8 @@ class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
+    @store = store
+    @prc = prc
   end
 
   def count
@@ -26,14 +28,50 @@ class BinaryMinHeap
 
   public
   def self.child_indices(len, parent_index)
-
+    if parent_index*2+1 > len-1
+      return nil
+    elsif parent_index*2+2 > len-1
+      return [2*parent_index+1]
+    else
+      return [2*parent_index+1,2*parent_index+2]
+    end
   end
 
   def self.parent_index(child_index)
-
+    parent = (child_index-1)/2
+    raise "root has no parent" if parent < 0
+    parent
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+
+    prc ||= Proc.new {|el,el2| el <=> el2 }
+    swap = true
+    while swap
+      swap = false
+      child_indexes = BinaryMinHeap.child_indices(len, parent_idx)
+      if child_indexes
+        if child_indexes.length > 1
+          if prc.call(child_indexes[0],child_indexes[1]) >= 0
+            swap_idx = child_indexes[1]
+          else
+            swap_idx = child_indexes[0]
+          end
+        else
+          swap_idx = child_indexes[0]
+        end
+      else
+        swap_idx = nil
+      end
+
+      if swap_idx && prc.call(array[parent_idx],array[swap_idx]) > 0
+        array[swap_idx], array[parent_idx] = array[parent_idx], array[swap_idx]
+        swap = true
+      end
+
+      parent_idx = swap_idx
+    end
+    array
 
   end
 
@@ -42,7 +80,7 @@ class BinaryMinHeap
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
-    
+
   end
 
 end

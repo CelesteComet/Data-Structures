57b1f9d45b528fc6754874137935cae3434d6bd1
bvluong <bryantvinluong@gmail.com>
2017-06-28 10:53:09 -0700

    Add Linked List

--- data_struc/lib/p04_linked_list.rb
+++ data_struc/lib/p04_linked_list.rb
@@ -2,11 +2,14 @@ class Node
   attr_accessor :key, :val, :next, :prev
 
   def initialize(key = nil, val = nil)
-
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
   end
 
   def to_s
-
+    "#{@key}: #{@val}"
   end
 
   def remove
@@ -18,30 +21,53 @@ class LinkedList
   include Enumerable
 
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
+
   end
 
   def first
+    @head.next
   end
 
   def last
-
+    @tail.prev
   end
 
   def empty?
-
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      p node
+      return node[key] if node.key == key
+    end
   end
 
   def include?(key)
-
+    self.each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    node = Node.new(key,val)
+    if empty?
+      @head.next, node.prev = node, @head
+      @tail.prev, node.next = node, @tail
+    else
+      old_node = @tail.prev
+      old_node.next, node.prev = node, old_node
+      @tail.prev, node.next = node, @tail
+    end
+
   end
 
   def update(key, val)
@@ -52,9 +78,15 @@ class LinkedList
   end
 
   def each
+    node = @head.next
+    until node == @tail
+      yield(node)
+      node = node.next
+    end
+
   end
 
-  # def to_s
-  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  end
 end

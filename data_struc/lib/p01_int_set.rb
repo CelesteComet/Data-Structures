cca95e2f916cdff1036ef89ad06302c507e56124
bvluong <bryantvinluong@gmail.com>
2017-06-27 21:32:05 -0700

    Create intset, maxintset, and resizingIntSet

--- data_struc/lib/p01_int_set.rb
+++ data_struc/lib/p01_int_set.rb
@@ -1,20 +1,26 @@
 class MaxIntSet
   def initialize(max)
-
+    @max = max
+    @store = Array.new(max)
   end
 
   def insert(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = true unless self.include?(num)
   end
 
   def remove(num)
+    @store[num] = false if self.include?(num)
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num <= @max && num >= 0
   end
 
   def validate!(num)
@@ -23,25 +29,31 @@ end
 
 class IntSet
   def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) {Array.new}
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-
+    @store[num%num_buckets]
   end
 
   def num_buckets
-
+    @store.length
   end
 end
 
@@ -49,29 +61,45 @@ class ResizingIntSet
   attr_reader :count
 
   def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) {Array.new}
+    @count = 0
   end
 
   def insert(num)
+    @count += 1
+    self[num] << num
+    resize! if count > num_buckets
+
+
   end
 
   def remove(num)
-
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
-
+    self[num].include?(num)
   end
 
   private
 
   def num_buckets
-
+    @store.length
   end
 
   def resize!
+    old_store = @store.dup
+    @store = Array.new(num_buckets*2) {Array.new}
+    @count = 0
+    old_store.flatten.each do |val|
+      self.insert(val)
+    end
   end
 
   def [](num)
-
+    @store[num%num_buckets]
   end
 end

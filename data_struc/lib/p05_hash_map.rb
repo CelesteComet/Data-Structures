b13aa43c5eb6078a46afdf73348bada8b1fad207
bvluong <bryantvinluong@gmail.com>
2017-08-01 13:50:38 -0700

    Add set, get, include methods to HashMap

--- data_struc/lib/p05_hash_map.rb
+++ data_struc/lib/p05_hash_map.rb
@@ -7,17 +7,25 @@ class HashMap
   attr_reader :count
 
   def initialize(num_buckets = 8)
-
+    @store = Array.new(num_buckets) { LinkedList.new }
   end
 
   def include?(key)
+    @store[bucket(key)].include?(key)
   end
 
   def set(key, val)
+    if include?(key)
+      @store[bucket(key)].update(key,val)
+    else
+      @store[bucket(key)].append(key,val)
+    end
   end
 
   def get(key)
-
+    if include?(key)
+      @store[bucket(key)].get(key)
+    end
   end
 
   def delete(key)
@@ -40,11 +48,13 @@ class HashMap
   private
 
   def num_buckets
+    @store.length
   end
 
   def resize!
   end
 
   def bucket(key)
+    key.hash % num_buckets
   end
 end

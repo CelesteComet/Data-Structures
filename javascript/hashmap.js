7e15c6b6fd32ed207b07c4d04e0d830954032279
bvluong <bryantvinluong@gmail.com>
2017-08-13 16:09:05 -0700

    Add skeleton for LinkedList class and Node class

--- javascript/hashmap.js
+++ javascript/hashmap.js
@@ -1,4 +1,47 @@
 
+class LinkedList {
+  constructor() {
+    this.head = new Node(null, null);
+    this.tail = new Node(null, null);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  first() {
+
+  }
+
+  last() {
+
+  }
+
+  add() {
+
+  }
+
+  get() {
+
+  }
+
+  remove() {
+
+  }
+
+}
+
+class Node {
+  constructor(key,val) {
+    this.key = key;
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+
+  remove() {
+  }
+
+}
+
 class HashMap {
   constructor() {
     this.store = Array(8);
@@ -7,7 +50,20 @@ class HashMap {
   }
 
   insert(key,val) {
+    // if (this.store[this.hash(key)]) {
+    //
+    // }
+  }
 
+  hash(key) {
+    var hash = 0, i, chr;
+    if (key.length === 0) return hash;
+    for (i = 0; i < key.length; i++) {
+      chr = key.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0;
+    }
+    return hash % this.length;
   }
 
   remove(key) {
@@ -21,14 +77,17 @@ class HashMap {
   resize() {
 
   }
+
+
 }
 
-let newHash = new HashMap
-newHash.insert("abc",2)
-newHash.insert("bcd",4)
-newHash.insert("asdf",5)
-newHash.insert("abc",1)
+let newHash = new HashMap;
+newHash.insert("abc",2);
+newHash.insert("bcd",4);
+newHash.insert("asdf",5);
+newHash.insert("abc",1);
 
 console.log(newHash['abc']);
 console.log(newHash['bcd']);
 console.log(newHash['asdf']);
+console.log(newHash.hash('abc'));
